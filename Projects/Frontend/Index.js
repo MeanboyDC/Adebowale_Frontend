9e0b61e5a4b4ec04114aef46ebe690d01b533156
Meanboy <adebow23@gmail.com>
2024-03-28 17:29:49 +0100

    Java intro

--- Projects/Frontend/Index.js
+++ Projects/Frontend/Index.js
@@ -1,1 +1,75 @@
-document addEventListener
+// strings
+// arrays
+// objects
+// inf
+// biolean
+// inf
+
+greetings = "Good afternoon"
+
+const greetings_default = "Hey"
+
+console.log(greetings)
+console.log(greetings_default)
+
+let income = 1000
+let expense = 900
+
+const total = income - expense
+
+console.log("Balance = " + total)
+
+let savings = 100
+let investment = 200
+
+if(savings > investment){
+    console.log("savings is greater than investment")
+
+} 
+else{
+    console.log("savings is not greater than investment")
+}
+
+let inflation = true
+let deflation = false
+
+if(inflation == true){
+    console.log("inflation dey oo")
+}
+else{
+    console.log("zero inflation")
+}
+
+bat = {
+    name: "tinubu",
+    age: "dunno",
+    role: "president",
+    nick: "balabulu"
+
+}
+
+bubu = {
+    name: "buhari",
+    age: "dunno",
+    role: "ex president",
+    nick: "bubu"
+}
+console.log(bubu)
+console.log(bat.role)
+
+const Paragraph = document.getElementById('Paragraph')
+
+const button = document.getElementById('button')
+button.addEventListener('click', function(){
+    Paragraph.textContent = "Try again nigga"
+})
+
+
+
+// Change content, modifies structure
+// Change styling
+// Add or Remove Elelments
+// Respond to event (click e.t.c)
+// Access Elements 
+// FAMILIES OF DOCUMENT CLASS
+
---
+++ Projects/Frontend/index2.js
@@ -0,0 +1,61 @@
+// strings
+// numbers
+// inf
+// biolean
+// objects
+// arrays
+
+// How are you this morning 
+// how are you this afternoon
+// how are you this evening
+
+let greetings = "How are you this morning"
+
+const greetings_default = "Hi welcome back"
+
+let x = 500
+let y = 200
+const total = x + y
+
+console.log(greetings_default)
+console.log("The total is" +  total)
+
+let class1 = 500
+let class2 = 1000
+
+if (class1 > class2){
+    console.log("class1 is greater than class2")
+}
+else{
+    console.log("class1 is not greater than class2")
+
+}
+
+let class3 = false
+let class4 = true
+
+if (class3 == false){
+    console.log("class3 is false")
+}
+else{
+    console.log("class4 is not false")
+}
+
+suv1 = {
+    name: "Tesla",
+    color: "Black",
+    model: "svg",
+    price: "200,000,000",
+    weight: "200kg"
+}
+
+ 
+suv2 = {
+    name:"Toyota",
+    color: "Black",
+    model: "rav4",
+    price: "100,000,000",
+    weight: "200kg"
+}
+console.log(suv1.price)
+console.log(suv2)
